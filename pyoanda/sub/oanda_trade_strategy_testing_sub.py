df6775d457646bb3920e6abf765cc8f649f33385
iamlxb3 <iamlxb3@gmail.com>
2017-03-02 13:38:51 +0000

    add oanda_trade_strategy_testing_sub

--- pyoanda/sub/oanda_trade_strategy_testing_sub.py
+++ pyoanda/sub/oanda_trade_strategy_testing_sub.py
@@ -13,7 +13,9 @@ def find_upper_level_folder_path(num, path=''):
 # import subprocess
 import os
 import sys
+import re
 import subprocess
+import collections
 
 
 def run_python(path):
@@ -21,13 +23,31 @@ def run_python(path):
     subprocess.call("python {}".format(path), shell=True, cwd=cwd)
 
 
+pyoanda_path = find_upper_level_folder_path(2)
+chosen_chromosome_path = os.path.join(pyoanda_path, 'best_chromosome', 'chosen_chromosome.txt')
+chosen_chromosome_dict = collections.defaultdict(lambda:[])
+# chromosome_combo_list = [(1_c1, 3_c1, 7_c1), (1_c2, 3_c2, 7_c2), ...]
+chromosome_combo_list = []
+chromosome_in_use_list = [(True, True, True), (True, True, False), (True, False, True),
+                          (False, True, True), (True, False, False), (False, True, False),
+                          (False, False, True)
+                          ]
+# read the chosen chromosome of 1,3,7 days
+with open(chosen_chromosome_path, 'r', encoding = 'utf-8') as f:
+    for line in f:
+        day = re.findall(r'#([0-9]+)#', line)[0]
+        chromosome = re.findall(r'#([0-9\,]+)#END#', line)[0]
+        chromosome_list = chromosome.split(',')
+        chosen_chromosome_dict[day].append(chromosome_list)
 
+print (chosen_chromosome_dict)
 
-
-
-
-
-
+for chromosome_1 in chosen_chromosome_dict['1']:
+    for chromosome_3 in chosen_chromosome_dict['3']:
+        for chromosome_7 in chosen_chromosome_dict['7']:
+            chromosome_combo = (chromosome_1, chromosome_3, chromosome_7)
+            print(chromosome_combo)
+            chromosome_combo_list.append(chromosome_combo)
 
 # # create path for python folder and files
 # # (1.) reader
